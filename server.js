e75dede79e43377b4c2c21453b099ea41789583c
Louis Lohmer <Louis.Lohmer@mediaman.de>
2025-09-02 19:23:49 +0200

    add databank connection and update readMe

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'databank-fitness-challenge-app',
+});
+
+// DO NOT USE: Endpoint to test connectifiy of databank
+app.get('/', (req, res) => {
+    connection.query('SELECT NOW() AS currentTime', (err, rows) => {
+        if (err) {
+            console.error('Fehler:', err);
+            res.status(500).send('Datenbank-Fehler');
+            return;
+        }
+        res.send('Verbindung erfolgreich! Aktuelle Zeit: ' + rows[0].currentTime);
+    });
+});
+
+app.listen(port, () => {
+    console.log('server runs on http://localhost:' + port);
+});
