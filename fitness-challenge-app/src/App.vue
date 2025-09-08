20065e65e42d12aa8f024b4516d2ec239aac36a1
Louis Lohmer <Louis.Lohmer@mediaman.de>
2025-09-08 19:17:03 +0200

    add frontend for dashboard

--- fitness-challenge-app/src/App.vue
+++ fitness-challenge-app/src/App.vue
@@ -1,20 +1,31 @@
 <script setup></script>
 
 <template>
-    <!-- Necessary to show active root -->
+    <!-- DO NOT REMOVE: Necessary to show active root -->
     <router-view></router-view>
 </template>
 
 <style>
 /* global styles */
 :root {
+    /* colors */
     --white: #f9f9f9;
+    --grey: #ddd;
+    --grey-2: #e7edff;
     --light-blue: #38bdf8;
     --light-blue-2: #3b82f6;
+    --light-green: #7fff00;
+    --light-green-2: #32cd32;
     --black: #000000;
     --red: #ff0000;
+
+    /* font-sizes */
+    --font-size-big-text: 24px;
     --font-size-text: 19px;
     --font-size-small-text: 16px;
+
+    /* font-weight */
+    --font-weight-bold: 900;
 }
 
 body {
--- fitness-challenge-app/src/Dashboard.vue
+++ fitness-challenge-app/src/Dashboard.vue
@@ -1,7 +1,149 @@
 <script setup></script>
 
 <template>
-    <h1>Dashboard.vue</h1>
+    <div class="dashboard-container">
+        <header>
+            <nav>
+                <h1>Dashboard</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" class="user-icon">
+                    <defs>
+                        <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
+                            <stop offset="0%" style="stop-color: #4facfe; stop-opacity: 1" />
+                            <stop offset="100%" style="stop-color: #00f2fe; stop-opacity: 1" />
+                        </linearGradient>
+                    </defs>
+                    <circle cx="64" cy="64" r="64" fill="url(#grad)" />
+                    <circle cx="64" cy="40" r="20" fill="white" />
+                    <path d="M32 96c0-17.67 14.33-32 32-32s32 14.33 32 32H32z" fill="white" />
+                </svg>
+            </nav>
+        </header>
+
+        <div class="active-challenges-card">
+            <h3>Aktive Challenges:</h3>
+            <!-- To-Do: Set dynamic number -->
+            <span class="challenge-counter">4</span>
+        </div>
+
+        <div class="groups">
+            <!-- To-Do: Set dynamic data -->
+            <div class="group">
+                <h3>Freunde Laufclub</h3>
+                <p>3 aktive Challenges</p>
+                <div class="progress-bar">
+                    <div class="progress"></div>
+                </div>
+                <p class="progress-text">Team Fortschritt: 65%</p>
+                <button class="group-button">Zur Gruppe</button>
+            </div>
+
+            <div class="new-group-card">
+                <div>
+                    <h3>Neue Gruppe</h3>
+                    <p>Erstelle eine private Gruppe</p>
+                </div>
+                <button class="create-group-button">+ Gruppe erstellen</button>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.user-icon {
+    margin-top: 15px;
+    width: 60px;
+    height: 60px;
+    cursor: pointer;
+}
+
+.challenge-counter {
+    font-size: var(--font-size-big-text);
+}
+
+.dashboard-container {
+    max-width: 1200px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+header nav {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.active-challenges-card {
+    background-color: var(--grey-2);
+    padding: 20px;
+    border-radius: 20px;
+    width: fit-content;
+}
+
+.groups {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+
+.group {
+    padding: 20px;
+    border-radius: 20px;
+    color: var(--white);
+    display: flex;
+    flex-direction: column;
+    background: linear-gradient(to bottom right, var(--light-blue), var(--light-blue-2));
+}
+
+.progress-bar {
+    background-color: var(--grey);
+    border-radius: 10px;
+    height: 10px;
+    margin: 10px 0;
+}
+
+.progress {
+    background: linear-gradient(to right, var(--light-green-2), var(--light-green));
+    height: 100%;
+    border-radius: 10px;
+    width: 65%;
+}
+
+.progress-text {
+    color: var(--white);
+    font-size: var(--font-size-small-text);
+}
+
+.group-button {
+    margin-top: auto;
+    font-weight: var(--font-weight-bold);
+    padding: 10px;
+    background-color: var(--grey);
+    color: var(--black);
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+}
+
+.new-group-card {
+    background-color: var(--white);
+    border: 1px solid var(--grey);
+    padding: 20px;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.create-group-button {
+    background: linear-gradient(to bottom right, var(--light-blue), var(--light-blue-2));
+    border: none;
+    padding: 10px;
+    color: var(--white);
+    font-weight: var(--font-weight-bold);
+    border-radius: 10px;
+    cursor: pointer;
+}
+</style>
